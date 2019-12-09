4226712c9cab714a8a7738598d17ebb6b81aae6b
Topodo <ariel.tirado@usach.cl>
2019-12-09 01:27:39 -0300

    first commit

---
+++ db/migrate/20191207210456_create_posts.rb
@@ -0,0 +1,11 @@
+class CreatePosts < ActiveRecord::Migration[5.1]
+  def change
+    create_table :posts do |t|
+      t.string :title
+      t.text :body
+      t.integer :visits_count
+
+      t.timestamps
+    end
+  end
+end
